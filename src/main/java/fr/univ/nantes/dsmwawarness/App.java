309c44f566525f623e05d28b0d1607c846b03392
Khaled <kmobayed@gmail.com>
2010-10-07 11:51:16 +0200

    comment out pull method

--- src/main/java/fr/univ/nantes/dsmwawarness/App.java
+++ src/main/java/fr/univ/nantes/dsmwawarness/App.java
@@ -1,13 +1,27 @@
 package fr.univ.nantes.dsmwawarness;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length<4)
+        {
+                System.err.println("Usage: java -jar loadData <TDB folder> <ontology file> <data file> <output file>");
+                System.exit(0);
+        }
+
+        String directory = args[0] ;
+        String ontoFile = "file:"+args[1];
+        String dataFile = "file:"+args[2];
+        String outputFile = args[3];
+        
+        Jena J= new Jena(directory);
+
+        J.loadData(ontoFile, dataFile);
+        J.extractKnows(outputFile);
+
+        J.close();
     }
+
+    
 }
